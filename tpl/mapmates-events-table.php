58cde1a354c02679be7b90f6d03146650827c863
Tate Needham <tate@t8software.com>
2018-03-06 10:30:58 +1100

    Added map mates event display

---
+++ tpl/mapmates-events-table.php
@@ -0,0 +1,21 @@
+<?php if (count($events)) { ?>
+	<table class="eventor zebra">
+		<thead>
+		<tr>
+			<th>Day</th>
+			<th>Date</th>
+			<th>Event</th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php foreach ($events as $e) { ?>
+			<tr>
+				<td><?php echo $e->weekday; ?></td>
+				<td class='evt-date'><?php echo $e->date; ?></td>
+				<td><a href='/eventor/events/details/<?php echo $e->id; ?>'><?php echo $e->title; ?></a></td>
+			</tr>
+		<?php } ?>
+		</tbody>
+	</table>
+<?php } ?>
+<a href="<?php echo add_query_arg('resetcache', 'yes');?>">Refresh data</a>
